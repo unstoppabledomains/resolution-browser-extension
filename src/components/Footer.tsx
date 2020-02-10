e365d4a471060af41a695b4f85df4d1591361df7
Johnny <johnny@unstoppabledomains.com>
2020-02-09 22:38:00 -0800

    link to all websites list

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
-import { Paper, WithStyles, withStyles, Grid, Typography } from '@material-ui/core';
+import { Paper, WithStyles, withStyles, Grid, Typography, Link } from '@material-ui/core';
 import styles from '../styles/footer.style';
 
 interface Props extends WithStyles<typeof styles>{
 }
 
-const Footer:React.FC<Props> = ({classes}) => (
-  <div className={classes.main}>
-    <Grid container wrap="nowrap" spacing={1}>
-      <Grid item>
-        <i className="material-icons md-24">folder</i>
+const websitesListURL = 'https://ipfs.github.io/public-gateway-checker/';
+
+const Footer:React.FC<Props> = ({classes}) =>  {
+  const navigateToList = () => {
+    return chrome.tabs.update({url: websitesListURL})
+  }
+
+  return (
+    <div className={classes.main}>
+      <Grid container wrap="nowrap" spacing={1}>
+        <Grid item>
+          <i className="material-icons md-24">folder</i>
+        </Grid>
+        <Grid item xs zeroMinWidth>
+        
+            <Link onClick={navigateToList}><Typography variant="subtitle1" className={classes.title}>
+              View list of websites
+            </Typography>
+            </Link>
+          
+        </Grid>
+        <Grid item className={classes.trailing}>
+        <i className="material-icons md-24">home</i>
+        </Grid>
       </Grid>
-      <Grid item xs zeroMinWidth>
-        <Typography variant="subtitle1" className={classes.title}>View list of websites</Typography>
-      </Grid>
-      <Grid item className={classes.trailing}>
-      <i className="material-icons md-24">home</i>
-      </Grid>
-    </Grid>
-  </div>
-)
+    </div>
+  );
+}
 
 export default withStyles(styles)(Footer);
