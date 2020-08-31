953ed90888243fcc54e6f5319a878b0eec766b05
sudoryan <rleinbox@gmail.com>
2020-08-31 10:42:04 -0700

    remove .bkp

--- scripts/firefox_manifest_creation.sh
+++ scripts/firefox_manifest_creation.sh
@@ -8,6 +8,5 @@ echo "making a manifest copy"
 cp $ORIGINAL_MANIFEST $STATIC_DIR/firefox_manifest.json
 ## Add gecko key to the end of json
 echo "appending gecko key to fireforx_manifest.json"
-sed -i .bkp -e '$ s/}/,\"applications\":{"gecko":{\"id\": \"ryan@unstoppabledomains.com\"}}}/g' static/firefox_manifest.json
+sed -i -e '$ s/}/,\"applications\":{"gecko":{\"id\": \"ryan@unstoppabledomains.com\"}}}/g' static/firefox_manifest.json
 echo "cleaning up"
-rm -r $STATIC_DIR/firefox_manifest.json.bkp
