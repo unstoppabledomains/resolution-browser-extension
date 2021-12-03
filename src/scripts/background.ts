ef3ad2ef4e91997dac66ae03094d33a4693fe310
Gentelman John <jeyhunt@gmail.com>
2021-12-03 09:22:29 -0800

    dev build option

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -3,10 +3,15 @@ import isValidDNSHostname from '../util/isValidDNSHostname';
 import { redirectToIpfs, supportedDomain, supportedDomains } from '../util/helpers'
 import { searchEngines, SearchEngine } from '../util/searchEngines'
 import OAURL from '../util/OsAgnosticURL';
+import logger from '../util/logger';
+
 chrome.webRequest.onBeforeRequest.addListener(
   requestDetails => {
+    logger.log("catched request");
+    logger.log({requestDetails});
     const url = new URL(requestDetails.url)
     const searchEngine = searchEngines.find(se => url.hostname.includes(se.hostname));
+    logger.log({searchEngine});
     if (!searchEngine) return
 
     const params = url.searchParams
@@ -14,6 +19,9 @@ chrome.webRequest.onBeforeRequest.addListener(
       .trim()
       .toLowerCase()
     const q = new OAURL(url.protocol + '//' + params)
+    logger.log({
+      params
+    });
     if (
       !q.hostname() ||
       !isValidDNSHostname(q.hostname()) ||
@@ -21,8 +29,8 @@ chrome.webRequest.onBeforeRequest.addListener(
     ) {
       return
     }
-
     if (q.hostname().endsWith('.888')) {
+      logger.log("domain does ends with .888");
       chrome.tabs.update(
         { url: 'index.html#loading' },
         async (tab: chrome.tabs.Tab) => {
@@ -32,6 +40,7 @@ chrome.webRequest.onBeforeRequest.addListener(
       )
       return {cancel: true}
     }
+    logger.log("domain doesn't end with .888");
     chrome.tabs.update({ url: q.toString() })
     return { cancel: true }
   },
@@ -47,6 +56,8 @@ chrome.webRequest.onBeforeRequest.addListener(
     chrome.tabs.update(
       { url: 'index.html#loading' },
       async (tab: chrome.tabs.Tab) => {
+          logger.log("catched request");
+          logger.log({requestDetails});
           await redirectToIpfs(requestDetails.url, tab.id)
         return { cancel: true }
       },
