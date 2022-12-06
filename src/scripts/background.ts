9625d8c5afe680ef2f41c9abab0a7abaffb02f44
Tu Nguyen <tu@unstoppabledomains.com>
2022-12-06 10:20:21 -0500

    Added declarative routes

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,71 +1,50 @@
 import '../subscripts/onInstalled'
-import isValidDNSHostname from '../util/isValidDNSHostname';
-import { redirectToIpfs, supportedDomain, supportedDomains } from '../util/helpers'
-import { searchEngines, SearchEngine } from '../util/searchEngines'
-import OAURL from '../util/OsAgnosticURL';
-import logger from '../util/logger';
+import isValidDNSHostname from '../util/isValidDNSHostname'
+import {
+  redirectToIpfs,
+  supportedDomain,
+  supportedDomains,
+} from '../util/helpers'
+import {searchEngines, SearchEngine} from '../util/searchEngines'
+import OAURL from '../util/OsAgnosticURL'
+import logger from '../util/logger'
 
-chrome.webRequest.onBeforeRequest.addListener(
-  requestDetails => {
-    logger.log("catched request");
-    logger.log({requestDetails});
-    const url = new URL(requestDetails.url)
-    const searchEngine = searchEngines.find(se => url.hostname.includes(se.hostname));
-    logger.log({searchEngine});
-    if (!searchEngine) return
-
-    const params = url.searchParams
-      .get(searchEngine.param)
-      .trim()
-      .toLowerCase()
-    const q = new OAURL(url.protocol + '//' + params)
-    logger.log({
-      params
-    });
-    if (
-      !q.hostname() ||
-      !isValidDNSHostname(q.hostname()) ||
-      !supportedDomain(q.hostname())
-    ) {
-      return
-    }
-    if (q.hostname().endsWith('.888')) {
-      logger.log("domain does ends with .888");
-      chrome.tabs.update(
-        { url: 'index.html#loading' },
-        async (tab: chrome.tabs.Tab) => {
-            await redirectToIpfs(q.toString(), tab.id)
-          return { cancel: true }
+chrome.declarativeNetRequest.updateDynamicRules(
+  {
+    addRules: [
+      {
+        id: 1001,
+        priority: 1,
+        action: {type: 'block'},
+        condition: {
+          urlFilter: 'google.com',
+          resourceTypes: ['main_frame'],
         },
-      )
-      return {cancel: true}
-    }
-    logger.log("domain doesn't end with .888");
-    chrome.tabs.update({ url: q.toString() })
-    return { cancel: true }
+      },
+    ],
+    removeRuleIds: [1001],
   },
-  {
-    urls: searchEngines.map((se: SearchEngine): string => `*://*.${se.hostname}/*`),
-    types: ['main_frame'],
-  },
-  ['blocking'],
+  console.log.bind(null, 'reachhhhhhhhh'),
 )
 
-chrome.webRequest.onBeforeRequest.addListener(
-  requestDetails => {
-    chrome.tabs.update(
-      { url: 'index.html#loading' },
-      async (tab: chrome.tabs.Tab) => {
-          logger.log("catched request");
-          logger.log({requestDetails});
-          await redirectToIpfs(requestDetails.url, tab.id)
-        return { cancel: true }
-      },
-    )
-  },
-  {
-    urls: supportedDomains.map((d: string): string => `*://*${d}/*`),
-    types: ['main_frame'],
-  },
-  ['blocking'],
-)
+supportedDomains.forEach((domain, index) => {
+  const id = index + 1001
+
+  chrome.declarativeNetRequest.updateDynamicRules(
+    {
+      addRules: [
+        {
+          id: id,
+          priority: 1,
+          action: {type: 'block'},
+          condition: {
+            urlFilter: domain,
+            resourceTypes: ['main_frame'],
+          },
+        },
+      ],
+      removeRuleIds: [id],
+    },
+    console.log.bind(null, `add rule ${id}`),
+  )
+})
