bdcee92c96d2809a4075f2009a85d3cbb4077379
Aaron Quirk <aquirk@gmail.com>
2024-08-14 11:08:37 -0400

    remove dead code

--- src/util/validations.ts
+++
@@ -1,5 +0,0 @@
-export const isValidEmail = (email: string): boolean => {
-  const atIndex = email.indexOf("@");
-  const dotIndex = email.lastIndexOf(".");
-  return atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < email.length - 1;
-};
